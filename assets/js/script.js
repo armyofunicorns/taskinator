54a5bd62e7c11fd7beb3ec59e253aa768fcb125b
Anthony Hall <anthony.w.hall@gmail.com>
2021-05-31 17:36:33 -0700

    Updates to the script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,17 @@
 
 var buttonEl = document.querySelector("#save-task");
 // console.log(buttonEl);
+
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function(taskTitle) {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = taskTitle;
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+buttonEl.addEventListener("click", function() {
+    let newTask = "Here is the new task";
+    createTaskHandler(newTask);
+});
