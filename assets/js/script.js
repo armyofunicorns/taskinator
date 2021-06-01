193ada2fc59ce5cfeb007f35e180057e5508419a
Anthony Hall <anthony.w.hall@gmail.com>
2021-05-31 22:42:47 -0700

    Cleared the fields after submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // console.dir(window.document);
-
 // var buttonEl = document.querySelector("#save-task");
 // console.log(buttonEl);
 
@@ -10,13 +9,32 @@ var createTaskHandler = function(event) {
     // Stops the default behavior to reload page
     event.preventDefault();
 
-    let newTaskContent = document.querySelector("#task-form").textContent;
-    console.log(newTaskContent);
+    // Capturing the values in Input and Select form fields
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    console.dir(taskNameInput);
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // Create a new li element and add a class
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = newTaskContent;
+
+    // Create a new div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    // Add some HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    // Append new div element
+    listItemEl.appendChild(taskInfoEl);
+    
+    // listItemEl.textContent = taskNameInput;
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
+
+    // Clear the field of the last input
+    document.getElementById("inputField").value = "";
+    document.getElementById("selectField").value = "";
+    
 };
 
 // buttonEl.addEventListener("click", function() {
