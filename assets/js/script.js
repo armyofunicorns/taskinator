16cf33572bef4f4615d1a8f5b9a967e7be7a7a92
Anthony Hall <anthony.w.hall@gmail.com>
2021-06-03 08:41:12 -0700

    Add to move the task to a new status

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,11 @@
 
 // Declare the counter
 let taskIdCounter = 1;
-
 let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
 let pageContentEl = document.querySelector("#page-content");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 let taskFormHandler = function(event) {
     // Stops the default behavior to reload page
@@ -179,7 +180,29 @@ let completeEditTask = function(taskName, taskType, taskId) {
     alert("Task Updated!");
 
     formEl.removeAttribute("data-task-id");
-    document.querySelector("#save-task").textContent = "Add Task";
+};
+
+let taskStatusChangeHandler = function(event) {
+    console.log(event.target);
+    console.log(even.target.getAttribute("data-task-id"));
+    // get the task item's id
+    let taskId = event.target.getAttribute("data-task-id");
+
+    // get the currently selected option's value and convert to lowercase
+    let statusValue = event.target.value.toLowerCase();
+
+    // find the parent task item element based on the id
+    let taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    if (statusValue === "to do") {
+        tasksToDoEl.appendChild(taskSelected);
+    } 
+    else if (statusValue === "in progress") {
+        tasksInProgressEl.appendChild(taskSelected);
+    } 
+    else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected);
+    }
 };
 
 // buttonEl.addEventListener("click", function() {
@@ -189,3 +212,4 @@ let completeEditTask = function(taskName, taskType, taskId) {
 
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
